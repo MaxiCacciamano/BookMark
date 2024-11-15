4d6d2b0a2d8a75cf974b81e016d11bc575d4c074
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-11-15 11:52:47 -0300

    99

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,14 +6,19 @@ const About = () => {
   return (
     <div className={style.manager}>
         <div style={{textAlign:'left', margin:'0 auto', width:'40%'}}>
-          <h1 style={{ lineHeight:'0px',fontSize:'40px', fontWeight:'600'}}>A Simple Bookmark</h1>
-          <h1 style={{ fontSize:'40px', fontWeight:'600'}}>Manager</h1>
-          <p style={{ marginBottom:'30px',width:'90%'}}>A clean and simple interface to organize your favourite website. Opera a new browser tab and see your site load instantly. Try it for free</p>
-          <button>Get in on Chrome</button>
-          <button>Get in on Firefox</button>
+          <h1 style={{ lineHeight:'0px',fontSize:'50px', fontWeight:'600'}}>A Simple Bookmark</h1>
+          <h1 style={{ fontSize:'50px', fontWeight:'600'}}>Manager</h1>
+          <p style={{ marginBottom:'30px',width:'90%', color:'hsl(229, 8%, 60%)'}}>A clean and simple interface to organize your favourite website. Opera a new browser tab and see your site load instantly. Try it for free</p>
+          <div className={style.buttons}>
+             <button>Get in on Chrome</button>
+             <button style={{backgroundColor:'white', color:'hsl(229, 8%, 60%)',boxShadow:'4px 4px 10px rgba(0, 0, 0, 0.2)'}}>Get in on Firefox</button>
+          </div>
         </div>
         <div className={style.image}>
-        <img src={tablett}/>
+           <img src={tablett}/>
+        </div>
+        <div className={style.background}>
+        .
         </div>
     </div>
   )
