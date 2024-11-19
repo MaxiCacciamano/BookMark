89cf3b291e14a0a70f5fe590c071576c17f31d88
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-11-19 10:14:36 -0300

    55

---
+++ src/components/Frequently.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from '../design/home.module.css'
+
+export const Frequently = () => {
+  return (
+    <div className={style.frquently}>
+        <h2>Frequently Asked Questions</h2>
+        <p>Here are some of our FAQ's. if you have any other questions 
+        you'd like answered please feel free to email us</p>
+    </div>
+  )
+}
